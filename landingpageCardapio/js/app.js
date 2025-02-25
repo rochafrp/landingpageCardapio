ae50f0064315d8afb15dab3e8979fbf09bac4d1f
rochafrp <rochafrp@gmail.com>
2025-02-25 15:46:50 -0300

    v1.6

--- landingpageCardapio/js/app.js
+++ landingpageCardapio/js/app.js
@@ -30,6 +30,7 @@ cardapio.metodos = {
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
             .replace(/\${nome}/g, e.name)
             .replace(/\${preco}/g, e.price.toFixed(2).replace(".", ","))
+            .replace(/\${id}/g, e.id)
 
             // botão ver mais foi clicado (12 itens)
             if (vermais && i >= 8 && i < 12) {
@@ -58,13 +59,32 @@ cardapio.metodos = {
         $("#btnVerMais").addClass('hidden');
     },
 
+    // diminuir a quantidade de intem no cardapio 
+    diminuirQuantidade: (id) => {
+
+        let qntAtual = parseInt ($("#qntd-" + id).text());
+
+        if (qntAtual > 0) {
+            $("#qntd-" + id).text(qntAtual - 1)
+        }
+
+    },
+
+    // aumentar a quantidade de intem no cardapio 
+    aumentarQuantidade: (id) => {
+
+        let qntAtual = parseInt ($("#qntd-" + id).text());
+        $("#qntd-" + id).text(qntAtual + 1)
+
+    }
+
 }
 
 cardapio.templates = {
 
     item: `
         <div class="col-3 mb-5">
-            <div class="card card-item">
+            <div class="card card-item" id="\${id}">
                 <div class="img-produto">
                     <img src="\${img}">
                 </div>
@@ -75,9 +95,9 @@ cardapio.templates = {
                     <b>R$ \${preco}</b>
                 </p>
                 <div class="add-carrinho">
-                    <span class="btn-menos"><i class="fa fa-minus"></i></span>
-                    <span class="add-numero-itens">0</span>
-                    <span class="btn-mais"><i class="fa fa-plus"></i></span>
+                    <span class="btn-menos" oneclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fa fa-minus"></i></span>
+                    <span class="add-numero-itens" id="qntd-\${id}">0</span>
+                    <span class="btn-mais" oneclick="cardapio.metodos.aumentarQuantidade('\${id}')><i class="fa fa-plus"></i></span>
                     <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
                 </div>
             </div>
